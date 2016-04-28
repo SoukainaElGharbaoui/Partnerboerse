e39d2282f2b8ee6ed7a39def97b235d13d8f6490
DunjaTomas <dt021@hdm-stuttgart.de>
2016-04-28 14:02:36 +0200

    client.partnerboerse.java bearbeitet für Testzwecke

--- Partnerboerse/src/de/hdm/gruppe7/partnerboerse/client/Partnerboerse.java
+++ Partnerboerse/src/de/hdm/gruppe7/partnerboerse/client/Partnerboerse.java
@@ -2,9 +2,14 @@ package de.hdm.gruppe7.partnerboerse.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.datepicker.client.DatePicker;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -28,6 +33,7 @@ public class Partnerboerse implements EntryPoint {
 		 * realisiert. Daher bietet sich ein VerticalPanel als Container an.
 		 */
 		VerticalPanel navPanel = new VerticalPanel();
+		HorizontalPanel horPanel = new HorizontalPanel();
 
 		/*
 		 * Das VerticalPanel wird einem DIV-Element namens "Navigator" in der
@@ -40,16 +46,9 @@ public class Partnerboerse implements EntryPoint {
 		 * Ab hier bauen wir sukzessive den Navigator mit seinen Buttons aus.
 		 */
 
-		/*
-		 * Neues Button Widget erzeugen und eine Beschriftung festlegen.
-		 */
-		final Button findNutzerprofilButton = new Button("Finde Nutzerprofil");
+		
 
-
-		/*
-		 * Hinzufügen des Buttons zum VerticalPanel.
-		 */
-		navPanel.add(findNutzerprofilButton);
+	
 		
 		 /*
 	     * Ab hier folgen weitere Button-Definitionen, die nach exakt der gleichen
@@ -61,19 +60,45 @@ public class Partnerboerse implements EntryPoint {
 	     * Hierauf wurde jedoch bewusst verzichtet, um den Komplexitätsgrad dieses
 	     * Demonstrators nicht unnötig zu erhöhen. 
 	     */
+		final TextBox vornameTextBox = new TextBox ();
+		final TextBox nachnameTextBox = new TextBox ();
+		final RadioButton weiblichRadioButton = new RadioButton("weiblich","weiblich");
+		final RadioButton maennlichRadioButton = new RadioButton("maennlich","maennlich");
+		final DatePicker geburtsdatumDatePicker = new DatePicker();
 	    final Button createNutzerprofilButton = new Button("Nutzerprofil anlegen");
 	    final Label vornameLabel = new Label("Vorname angeben");
 	    final Label nachnameLabel = new Label("Nachname angeben");
 	    final Label geburtsdatumLabel = new Label("Geburtsdatum angeben");
+	    final Label geschlechtLabel = new Label("Geschlecht auswaehlen");
+	    
+	    navPanel.add(horPanel);
+	    horPanel.add(vornameLabel);	
+	    horPanel.add(vornameTextBox);   
+	    navPanel.add(horPanel);
+		horPanel.add(nachnameLabel);	
+		horPanel.add(nachnameTextBox); 
+		navPanel.add(horPanel);
+		horPanel.add(geburtsdatumLabel);	
+	    horPanel.add(geburtsdatumDatePicker);
+	    navPanel.add(horPanel);
+	    horPanel.add(geschlechtLabel);
+	    horPanel.add(weiblichRadioButton);
+	    horPanel.add(maennlichRadioButton);
 	    navPanel.add(createNutzerprofilButton);
-	    navPanel.add(vornameLabel);	    
-	    navPanel.add(nachnameLabel);	    
-	    navPanel.add(geburtsdatumLabel);	    
+	    /** 
+	     * Nur ein Geschlecht w�hlbar
+	     */
+	    
+	    //weiblichRadioButton.setValue(true);
+	    if (weiblichRadioButton.getValue() == true){
+	    	maennlichRadioButton.setValue(false);
+	    	
+	    	
+	    }
 	    
 	    
-	 // Nächste Button-Definition
-	    final Button deleteNutzerprofilButton = new Button("Nutzerprofil löschen");
-	    navPanel.add(deleteNutzerprofilButton);
+	    
+	
 	    
 	}
 }
