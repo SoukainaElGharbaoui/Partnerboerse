356866ee91f3e7fe51633aaf1e22a520989965e6
CarolinMarian <carolin.marian95@googlemail.com>
2016-04-28 21:31:23 +0200

    Client Partnerboerse.java weitere Eingabefelder hinzugefügt

--- Partnerboerse/src/de/hdm/gruppe7/partnerboerse/client/Partnerboerse.java
+++ Partnerboerse/src/de/hdm/gruppe7/partnerboerse/client/Partnerboerse.java
@@ -5,10 +5,12 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.datepicker.client.DatePicker;
 
 /**
@@ -33,6 +35,7 @@ public class Partnerboerse implements EntryPoint {
 		 * realisiert. Daher bietet sich ein VerticalPanel als Container an.
 		 */
 		VerticalPanel navPanel = new VerticalPanel();
+
 		HorizontalPanel horPanel = new HorizontalPanel();
 
 		/*
@@ -41,7 +44,9 @@ public class Partnerboerse implements EntryPoint {
 		 * Darstellungsort.
 		 */
 		RootPanel.get("Navigator").add(navPanel);
+	
 
+		
 		/*
 		 * Ab hier bauen wir sukzessive den Navigator mit seinen Buttons aus.
 		 */
@@ -60,45 +65,94 @@ public class Partnerboerse implements EntryPoint {
 	     * Hierauf wurde jedoch bewusst verzichtet, um den KomplexitÃ¤tsgrad dieses
 	     * Demonstrators nicht unnÃ¶tig zu erhÃ¶hen. 
 	     */
+		
+		/**
+		 * Erzeugen eines EIngabefelds für den Vornamen
+		 */
 		final TextBox vornameTextBox = new TextBox ();
+		final Label vornameLabel = new Label("Vorname angeben");
+		navPanel.add(vornameLabel);	
+	    navPanel.add(vornameTextBox);
+		
+	    
+	    /**
+		 * Erzeugen eines EIngabefelds für den Nachnamen
+		 */
 		final TextBox nachnameTextBox = new TextBox ();
+		final Label nachnameLabel = new Label("Nachname angeben");
+		navPanel.add(nachnameLabel);	
+		navPanel.add(nachnameTextBox);
+		
+		/**
+		 * Erzeugen einer Auswahl für das Geschlecht
+		 */
+		final Label geschlechtLabel = new Label("Geschlecht auswaehlen");
 		final RadioButton weiblichRadioButton = new RadioButton("weiblich","weiblich");
 		final RadioButton maennlichRadioButton = new RadioButton("maennlich","maennlich");
-		final DatePicker geburtsdatumDatePicker = new DatePicker();
-	    final Button createNutzerprofilButton = new Button("Nutzerprofil anlegen");
-	    final Label vornameLabel = new Label("Vorname angeben");
-	    final Label nachnameLabel = new Label("Nachname angeben");
-	    final Label geburtsdatumLabel = new Label("Geburtsdatum angeben");
-	    final Label geschlechtLabel = new Label("Geschlecht auswaehlen");
-	    
-	    navPanel.add(horPanel);
-	    horPanel.add(vornameLabel);	
-	    horPanel.add(vornameTextBox);   
-	    navPanel.add(horPanel);
-		horPanel.add(nachnameLabel);	
-		horPanel.add(nachnameTextBox); 
-		navPanel.add(horPanel);
-		horPanel.add(geburtsdatumLabel);	
-	    horPanel.add(geburtsdatumDatePicker);
-	    navPanel.add(horPanel);
-	    horPanel.add(geschlechtLabel);
-	    horPanel.add(weiblichRadioButton);
-	    horPanel.add(maennlichRadioButton);
-	    navPanel.add(createNutzerprofilButton);
-	    /** 
-	     * Nur ein Geschlecht wählbar
-	     */
-	    
-	    //weiblichRadioButton.setValue(true);
-	    if (weiblichRadioButton.getValue() == true){
-	    	maennlichRadioButton.setValue(false);
-	    	
-	    	
-	    }
-	    
-	    
-	    
+		navPanel.add(geschlechtLabel);
+	    navPanel.add(weiblichRadioButton);
+	    navPanel.add(maennlichRadioButton);
+//	    FUNKTIONIERT NICHT
+//	    if (weiblichRadioButton.getValue() == true){
+//	    	maennlichRadioButton.setValue(false);
+//	    }
+//	    
 	
+		/**
+		 * Erzeugen eines DatePickers für das Geburtsdatum
+		 */
+		final Label geburtsdatumLabel = new Label("Geburtsdatum angeben");
+		final DatePicker geburtsdatumDatePicker = new DatePicker();
+	    navPanel.add(geburtsdatumLabel);	
+	    navPanel.add(geburtsdatumDatePicker);
+	    
+		
+		/**
+		 * Erzeugen einer CheckBox für Raucher
+		 */
+		final Label raucherLabel = new Label("Rauchen Sie?");
+		final CheckBox raucherCheckBox = new CheckBox("Raucher");
+		final CheckBox nichtRaucherCheckBox = new CheckBox("Nichtraucher");
+		navPanel.add(raucherLabel);
+	    navPanel.add(raucherCheckBox);
+	    navPanel.add(nichtRaucherCheckBox);
+//	    FUNKTIONIERT NICHT
+//	    if (raucherCheckBox.getValue() == true){
+//	   	
+//	    	nichtRaucherCheckBox.setValue(false);
+//	    }	else if (nichtRaucherCheckBox.getValue() == true){
+//	    	raucherCheckBox.setValue(false);
+//	    }  	else {raucherCheckBox.setValue(false);
+//	        nichtRaucherCheckBox.setValue(false);
+//	    }
+	    
+	    /**
+		 * Erzeugen eines EIngabefelds für die Größe in cm
+		 */
+		final TextBox groesseTextBox = new TextBox ();
+		final Label groesseLabel = new Label("Groesse angeben");
+		navPanel.add(groesseLabel);	
+	    navPanel.add(groesseTextBox);
+	    
+	    /**
+		 * Erzeugen eines EIngabefelds für die Haarfarbe
+		 */
+		final ListBox haarfarbeListBox = new ListBox ();
+		final Label haarfarbeLabel = new Label("Haarfarbe angeben");
+		haarfarbeListBox.addItem("Blond");
+		haarfarbeListBox.addItem("Braun");
+		haarfarbeListBox.addItem("Rot");
+		haarfarbeListBox.addItem("Schwarz");
+		haarfarbeListBox.addItem("Grau");
+		haarfarbeListBox.addItem("Glatze");
+		navPanel.add(haarfarbeLabel);	
+		navPanel.add(haarfarbeListBox);
+	   
+	    /**
+		 * Erzugen eines Button "Nutzerprofil anlegen"
+		 */
+		final Button createNutzerprofilButton = new Button("Nutzerprofil anlegen");
+	    navPanel.add(createNutzerprofilButton);
 	    
 	}
 }
