2235cd631b8e2467e0030f9e3e63cab9fcec8703
SoukainaElGharbaoui <soukaina.el@arcor.de>
2016-05-05 20:50:20 +0200

    Methoden zu Auswahl-und Beschreibungseigenschaft hinzugefügt

--- Partnerboerse/src/de/hdm/gruppe7/partnerboerse/shared/bo/Auswahleigenschaft.java
+++ Partnerboerse/src/de/hdm/gruppe7/partnerboerse/shared/bo/Auswahleigenschaft.java
@@ -3,5 +3,25 @@ package de.hdm.gruppe7.partnerboerse.shared.bo;
 public class Auswahleigenschaft extends Eigenschaft {
 	
 	private static final long serialVersionUID = 1L;
+	
+	public String auswahltext;
+	//private List <Auswahloption> auswahloption;
+	
+	public void setAuswahltext(String auswahltext){
+		this.auswahltext = auswahltext;
+	}
+	
+	public String getAuswahltext(){
+		return auswahltext;
+	}
+	
+	//public void setZugehoerigeAuswahloptionen(List<Auswahloption> auswahloption){
+	//	this.auswahloption = auswahloption;
+	//}
+	
+	//public List <Auswahloption> getZugehoerigeAuswahloptionen(){
+	//	return auswahloption;
+		
+	}
 
-}
+
