edd0c54fab6bb1931a78a3dc432393dd8141bed6
SoukainaElGharbaoui <soukaina.el@arcor.de>
2016-05-07 19:30:41 +0200

    Auswahl- und Beschreibungseigenschaftsmapper erstellt

--- Partnerboerse/src/de/hdm/gruppe7/partnerboerse/shared/bo/Auswahleigenschaft.java
+++ Partnerboerse/src/de/hdm/gruppe7/partnerboerse/shared/bo/Auswahleigenschaft.java
@@ -1,11 +1,18 @@
 package de.hdm.gruppe7.partnerboerse.shared.bo;
 
+import java.util.List;
+
 public class Auswahleigenschaft extends Eigenschaft {
 	
 	private static final long serialVersionUID = 1L;
 	
 	public String auswahltext;
 	//private List <Auswahloption> auswahloption;
+	
+	
+	private List <Auswahloption> auswahloption;
+	
+	public List <Auswahloption> zugehoerigeAuswahloptionen;
 	
 	public void setAuswahltext(String auswahltext){
 		this.auswahltext = auswahltext;
@@ -15,13 +22,15 @@ public class Auswahleigenschaft extends Eigenschaft {
 		return auswahltext;
 	}
 	
-	//public void setZugehoerigeAuswahloptionen(List<Auswahloption> auswahloption){
-	//	this.auswahloption = auswahloption;
-	//}
+	public void setZugehoerigeAuswahloptionen(List<Auswahloption> auswahloption){
+		this.auswahloption = auswahloption;
+	}
 	
-	//public List <Auswahloption> getZugehoerigeAuswahloptionen(){
-	//	return auswahloption;
+	public List <Auswahloption> getZugehoerigeAuswahloptionen(){
+		return auswahloption;
 		
 	}
+	
+}
 
 
