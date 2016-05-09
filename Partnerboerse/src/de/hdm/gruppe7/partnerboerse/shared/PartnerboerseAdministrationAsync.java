4b7995ea0772b7b155b0272a538b394e634075a2
Nina <Nina@10.60.33.9>
2016-05-09 11:42:47 +0200

    Problembehebung

--- Partnerboerse/src/de/hdm/gruppe7/partnerboerse/shared/PartnerboerseAdministrationAsync.java
+++ Partnerboerse/src/de/hdm/gruppe7/partnerboerse/shared/PartnerboerseAdministrationAsync.java
@@ -1,40 +1,5 @@
 package de.hdm.gruppe7.partnerboerse.shared;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Vector;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-import de.hdm.gruppe7.partnerboerse.shared.bo.Merkliste;
-import de.hdm.gruppe7.partnerboerse.shared.bo.Nutzerprofil;
-
-public interface PartnerboerseAdministrationAsync {
-
-	void init(AsyncCallback<Void> callback);
-
-	void createNutzerprofil(String vorname, String nachname,
-			String geburtsdatum, String geschlecht, String haarfarbe,
-			String koerpergroesse, String raucher, String religion,
-			AsyncCallback<Nutzerprofil> callback);
-
-	void getNutzerprofilById(int profilId, AsyncCallback<Nutzerprofil> callback);
-
-	void save(Nutzerprofil nutzerprofil, AsyncCallback<Void> callback);
-
-	void delete(Nutzerprofil nutzerprofil, AsyncCallback<Void> callback);
-
-	void getAngeseheneNpFor(Nutzerprofil nutzerprofil,
-			AsyncCallback<List<Nutzerprofil>> callback);
-
-	/**
-	 * ABSCHNITT MERKLISTE: BEGINN
-	 */
-
-	void getGemerkteProfileFor(int profilId, AsyncCallback<Vector<Merkliste>> callback);
-
-	/**
-	 * ABSCHNITT MERKLISTE: ENDE
-	 */
+public class PartnerboerseAdministrationAsync {
 
 }
