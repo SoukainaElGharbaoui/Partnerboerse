00805a0c340abff277ffd898cd489cb9c36334c7
SoukainaElGharbaoui <soukaina.el@arcor.de>
2016-05-23 19:08:55 +0200

    Login-Klassen hinzugefügt

--- Partnerboerse/src/de/hdm/gruppe7/partnerboerse/client/LoginInfo.java
+++ Partnerboerse/src/de/hdm/gruppe7/partnerboerse/client/LoginInfo.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class LoginInfo implements Serializable {
 	
+	
+	private static final long serialVersionUID = 1L;
 	private boolean loggedIn = false;
 	private String loginUrl;
 	private String logoutUrl;
@@ -12,6 +14,43 @@ public class LoginInfo implements Serializable {
 	
 	public boolean isLoggedIn(){
 		return loggedIn;
+	}
+
+	public void setLoggedIn(boolean b) {
+		
+	}
+	
+	public boolean getLoggedIn(){
+		return loggedIn;
+	}
+	
+	public void setLogoutUrl(String createLogoutURL) {
+		
+	}
+	public String getLogoutUrl(){
+		return logoutUrl;
+	}
+
+	public void setNickname(String nickname2) {
+		
+	}
+	public String getNickname(){
+		return nickname;
+	}
+
+	public void setEmailAddress(String email) {
+		
+	}
+	
+	public String getEmailAddress(){
+		return emailAddress;
+	}
+
+	public void setLoginUrl(String createLoginURL) {
+		
+	}
+	public String getLoginUrl(){
+		return loginUrl;
 	}
 
 }
