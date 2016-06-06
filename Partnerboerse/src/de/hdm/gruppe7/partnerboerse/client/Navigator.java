165d5046609e8551cb51a61371bdcce87a7560de
Nina <Nina@10.60.40.217>
2016-06-06 11:36:35 +0200

    Merge

--- Partnerboerse/src/de/hdm/gruppe7/partnerboerse/client/Navigator.java
+++ Partnerboerse/src/de/hdm/gruppe7/partnerboerse/client/Navigator.java
@@ -32,43 +32,43 @@ import de.hdm.gruppe7.partnerboerse.shared.bo.Profil;
 
 import de.hdm.gruppe7.partnerboerse.shared.bo.Suchprofil;
 
-
 public class Navigator extends VerticalPanel {
-	
+
 	Nutzerprofil nutzerprofil = new Nutzerprofil();
 
 	int aehnlichkeit = 0;
 
 	public Navigator() {
 		VerticalPanel verPanel1 = new VerticalPanel();
-		
-//		Tree t = new Tree(); 
-//		
-//		TreeItem meinNutzerprofil = new TreeItem(); 
-//		meinNutzerprofil.setText("Mein Nutzerprofil");
-//		meinNutzerprofil.addTextItem("Meine Nutzerprofildaten"); 
-//		meinNutzerprofil.addTextItem("Meine Infos"); 
-//		
-//		TreeItem meineSuchprofile = new TreeItem(); 
-//		meineSuchprofile.setText("Meine Suchprofile");
-//		
-//		TreeItem andereNutzerprofile = new TreeItem(); 
-//		andereNutzerprofile.setText("Andere Nutzerprofile");
-//		andereNutzerprofile.addTextItem("Meine Merkliste"); 
-//		andereNutzerprofile.addTextItem("Meine Sperrliste"); 
-//		
-//		TreeItem meinePartnervorschlaege = new TreeItem(); 
-//		meinePartnervorschlaege.setText("Meine Partnervorschläge");
-//		meinePartnervorschlaege.addTextItem("Meine unangesehenen Partnervorschläge"); 
-//		meinePartnervorschlaege.addTextItem("Meine Partnervorschläge anhand von Suchprofilen"); 
-//		
-//		t.addItem(meinNutzerprofil); 
-//		t.addItem(meineSuchprofile);
-//		t.addItem(andereNutzerprofile);
-//		t.addItem(meinePartnervorschlaege); 
-//
-//		this.add(t); 
-		
+
+		// Tree t = new Tree();
+		//
+		// TreeItem meinNutzerprofil = new TreeItem();
+		// meinNutzerprofil.setText("Mein Nutzerprofil");
+		// meinNutzerprofil.addTextItem("Meine Nutzerprofildaten");
+		// meinNutzerprofil.addTextItem("Meine Infos");
+		//
+		// TreeItem meineSuchprofile = new TreeItem();
+		// meineSuchprofile.setText("Meine Suchprofile");
+		//
+		// TreeItem andereNutzerprofile = new TreeItem();
+		// andereNutzerprofile.setText("Andere Nutzerprofile");
+		// andereNutzerprofile.addTextItem("Meine Merkliste");
+		// andereNutzerprofile.addTextItem("Meine Sperrliste");
+		//
+		// TreeItem meinePartnervorschlaege = new TreeItem();
+		// meinePartnervorschlaege.setText("Meine Partnervorschläge");
+		// meinePartnervorschlaege.addTextItem("Meine unangesehenen
+		// Partnervorschläge");
+		// meinePartnervorschlaege.addTextItem("Meine Partnervorschläge anhand
+		// von Suchprofilen");
+		//
+		// t.addItem(meinNutzerprofil);
+		// t.addItem(meineSuchprofile);
+		// t.addItem(andereNutzerprofile);
+		// t.addItem(meinePartnervorschlaege);
+		//
+		// this.add(t);
 
 		/**
 		 * Button "Nutzerprofil anlegen" hinzufügen. !!! Gehört hier nicht hin,
@@ -106,31 +106,25 @@ public class Navigator extends VerticalPanel {
 		// }
 		//
 		// });
-		
+
 		/**
 		 * Information-Label hinzuf�gen.
 		 */
 		final Label infoLabel = new Label();
 
-
 		// logoutButton.setStyleName("navigatorbutton");
 		// this.add(logoutButton);
-		
+
 		final Button nutzerprofilAnlegenButton = new Button("Nutzerprofil anlegen");
 
 		nutzerprofilAnlegenButton.addClickHandler(new ClickHandler() {
 
 			public void onClick(ClickEvent event) {
-				
-				
+
 				CreateNutzerprofil createNutzerprofil = new CreateNutzerprofil();
 				RootPanel.get("Details").clear();
 				RootPanel.get("Details").add(createNutzerprofil);
-				
-				
-				
-			
-				
+
 			}
 		});
 
@@ -153,10 +147,6 @@ public class Navigator extends VerticalPanel {
 		showEigenesNpButton.setStyleName("navigatorbutton");
 		this.add(showEigenesNpButton);
 
-		
-
-
-		
 		/**
 		 * Button "Suchprofile anzeigen" hinzufügen.
 		 */
@@ -214,7 +204,6 @@ public class Navigator extends VerticalPanel {
 
 			public void onClick(ClickEvent event) {
 
-
 				ClientsideSettings.getPartnerboerseAdministration().getUnangeseheneNutzerprofile(
 
 						new AsyncCallback<List<Nutzerprofil>>() {
@@ -231,8 +220,8 @@ public class Navigator extends VerticalPanel {
 
 									final int fremdprofilId = np.getProfilId();
 
-									ClientsideSettings.getPartnerboerseAdministration().berechneAehnlichkeitNpFor(
-											fremdprofilId, new AsyncCallback<Integer>() {
+									ClientsideSettings.getPartnerboerseAdministration()
+											.berechneAehnlichkeitNpFor(fremdprofilId, new AsyncCallback<Integer>() {
 
 												@Override
 												public void onFailure(Throwable caught) {
@@ -276,104 +265,105 @@ public class Navigator extends VerticalPanel {
 
 						});
 
+				// Ab hier wird die Aehnlichkeit zwischen den Suchprofilen und
+				// den Nutzerprofilen errechnet
 
-				
-				//Ab hier wird die Aehnlichkeit zwischen den Suchprofilen und den Nutzerprofilen errechnet
-				
-//				ClientsideSettings.getPartnerboerseAdministration().getAllSuchprofileFor(Benutzer.getProfilId(), new AsyncCallback<List<Suchprofil>>() {
-//					
-//					
-//					@Override
-//					public void onFailure(Throwable caught) {
-//						infoLabel.setText("Es trat ein Fehler auf.");
-//					}
-//					
-//					@Override
-//					public void onSuccess(List<Suchprofil> result1) {
-//						
-//						for (Suchprofil sp : result1){
-//							
-//							final int suchprofilId = sp.getProfilId();							
-//				final String suchprofilName = sp.getSuchprofilName();
-//				
-//							ClientsideSettings.getPartnerboerseAdministration().getAllNutzerprofile(new AsyncCallback<List<Nutzerprofil>>(){
-//					
-//					@Override
-//					public void onFailure(Throwable caught) {
-//						infoLabel.setText("Es trat ein Fehler auf.");
-//						
-//					}
-//					
-//					@Override
-//					public void onSuccess(List<Nutzerprofil> result) {
-//						
-//												
-//				for (Nutzerprofil np : result){
-//								
-//								
-//				final int fremdprofilId = np.getProfilId();
-//				
-//				
-//				ClientsideSettings.getPartnerboerseAdministration().berechneAehnlichkeitSpFor(suchprofilId, fremdprofilId,  suchprofilName , new AsyncCallback<Integer>(){
-//
-//					@Override
-//					public void onFailure(Throwable caught) {
-//						infoLabel.setText("Es trat ein Fehler auf.");
-//					}
-//
-//					@Override
-//					public void onSuccess(Integer result3) {
-//						aehnlichkeit = result3;
-//						
-//						
-//						
-//						
-//						ClientsideSettings.getPartnerboerseAdministration().aehnlichkeitSetzenSp(Benutzer.getProfilId(), suchprofilName,  fremdprofilId, aehnlichkeit, new AsyncCallback<Void>(){
-//
-//							@Override
-//							public void onFailure(Throwable caught) {
-//								infoLabel.setText("Es trat ein Fehler auf.");
-//							}
-//
-//							@Override
-//							public void onSuccess(Void result4) {
-//								// TODO Auto-generated method stub
-//
-//																													}
-//
-//																												});
-//																							}
-//
-//																						});
-//
-//																	}
-//
-//																
-//															
-//
-//														
-//
-//									}
-//
-//								});
-//							
-//							
-//							
-//							
-//							
-//						}
-//						
-//						
-//						
-//						
-//					}
-//				});
-//				
-//				
-//				
-		
-				
-				
+				// ClientsideSettings.getPartnerboerseAdministration().getAllSuchprofileFor(Benutzer.getProfilId(),
+				// new AsyncCallback<List<Suchprofil>>() {
+				//
+				//
+				// @Override
+				// public void onFailure(Throwable caught) {
+				// infoLabel.setText("Es trat ein Fehler auf.");
+				// }
+				//
+				// @Override
+				// public void onSuccess(List<Suchprofil> result1) {
+				//
+				// for (Suchprofil sp : result1){
+				//
+				// final int suchprofilId = sp.getProfilId();
+				// final String suchprofilName = sp.getSuchprofilName();
+				//
+				// ClientsideSettings.getPartnerboerseAdministration().getAllNutzerprofile(new
+				// AsyncCallback<List<Nutzerprofil>>(){
+				//
+				// @Override
+				// public void onFailure(Throwable caught) {
+				// infoLabel.setText("Es trat ein Fehler auf.");
+				//
+				// }
+				//
+				// @Override
+				// public void onSuccess(List<Nutzerprofil> result) {
+				//
+				//
+				// for (Nutzerprofil np : result){
+				//
+				//
+				// final int fremdprofilId = np.getProfilId();
+				//
+				//
+				// ClientsideSettings.getPartnerboerseAdministration().berechneAehnlichkeitSpFor(suchprofilId,
+				// fremdprofilId, suchprofilName , new AsyncCallback<Integer>(){
+				//
+				// @Override
+				// public void onFailure(Throwable caught) {
+				// infoLabel.setText("Es trat ein Fehler auf.");
+				// }
+				//
+				// @Override
+				// public void onSuccess(Integer result3) {
+				// aehnlichkeit = result3;
+				//
+				//
+				//
+				//
+				// ClientsideSettings.getPartnerboerseAdministration().aehnlichkeitSetzenSp(Benutzer.getProfilId(),
+				// suchprofilName, fremdprofilId, aehnlichkeit, new
+				// AsyncCallback<Void>(){
+				//
+				// @Override
+				// public void onFailure(Throwable caught) {
+				// infoLabel.setText("Es trat ein Fehler auf.");
+				// }
+				//
+				// @Override
+				// public void onSuccess(Void result4) {
+				// // TODO Auto-generated method stub
+				//
+				// }
+				//
+				// });
+				// }
+				//
+				// });
+				//
+				// }
+				//
+				//
+				//
+				//
+				//
+				//
+				// }
+				//
+				// });
+				//
+				//
+				//
+				//
+				//
+				// }
+				//
+				//
+				//
+				//
+				// }
+				// });
+				//
+				//
+				//
 
 				ShowPartnervorschlaege showPartnervorschlaege = new ShowPartnervorschlaege();
 				RootPanel.get("Details").clear();
@@ -384,7 +374,7 @@ public class Navigator extends VerticalPanel {
 		});
 
 		verPanel1.add(infoLabel);
-		
+
 		showPartnervorschlaegeButton.setStyleName("navigatorbutton");
 		this.add(showPartnervorschlaegeButton);
 
